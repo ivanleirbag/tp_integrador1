0e6b05d43a4ae8d245007e0e1f38d446cef6582e
ivanleirbag <ivanpeceto20@gmail.com>
2025-04-18 19:19:13 -0300

    Primer aproximación al módulo weather

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from flask import Flask, render_template
 from routes.main_routes import main_bp
 from routes.number_routes import number_bp
-
+from routes.weather_routes import weather_bp
 
 app = Flask(__name__)
 app.register_blueprint(main_bp)
+app.register_blueprint(weather_bp, url_prefix="/weather")
 app.register_blueprint(number_bp, url_prefix='/number')
 
-
-# Manejo de errores tipo 404
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404_templates.html'), 404
     
-# Manejo de errores tipo 500
 @app.errorhandler(500)
 def internal_server_error(error):
     return render_template('500_templates.html'), 500
--- services/weather_service.py
+++ services/weather_service.py
@@ -1,18 +1,56 @@
-# NumberC.py
-from flask import Blueprint, render_template, request
-from zeep import Client
+import requests
+import os
+from dotenv import load_dotenv
 
-number_bp = Blueprint('number_bp', __name__)
+load_dotenv()
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
 
-client = Client('https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL')
+def get_coordinates(city_name):
+    url = "https://nominatim.openstreetmap.org/search"
+    params = {
+        "q": city_name,
+        "format": "json",
+        "limit": 1
+    }
+    headers = {
+        "User-Agent": "weather-app-flask"
+    }
 
-@number_bp.route('/', methods=['GET', 'POST'])
-def home():
-    result = None
-    if request.method == 'POST':
-        number = request.form.get('number')
-        if number.isdigit():
-            result = client.service.NumberToWords(ubiNum=int(number))
-        else:
-            result = "Por favor, ingresa un número válido."
-    return render_template('index.html', result=result)
+    try:
+        response = requests.get(url, params=params, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+
+        if not data:
+            return None, "Ciudad no encontrada"
+        
+        lat = data[0]["lat"]
+        lon = data[0]["lon"]
+        return (lat, lon), None
+    
+    except Exception as e:
+        return None, f"Error al buscar coordenadas: {e}"
+
+def get_weather(lat, lon):
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "appid": API_KEY,
+        "units": "metric",
+        "lang": "es"
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        data = response.json()
+        weather_info = {
+            "city": data["name"],
+            "temp": round(data["main"]["temp"], 1),
+            "description": data["weather"][0]["description"]
+        }
+        return weather_info, None
+    
+    except Exception as e:
+        return None, f"Error al consultar el clima: {e}"
