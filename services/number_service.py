4f1217bc4fb8aa41f278ce2918e0ca62b8a7d741
ivanleirbag <169113267+ivanleirbag@users.noreply.github.com>
2025-04-20 23:23:40 -0300

    Añadida la documentación en los modulos de servicios y scripts de js

--- services/number_service.py
+++ services/number_service.py
@@ -3,6 +3,16 @@ from zeep import Client
 client = Client('https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL')
 
 def convert_number_to_words(number):
+    """
+    @brief Converts a numeric string into its word representation using the NumberConversion SOAP service.
+
+    @param number A string representing the number to be converted.
+
+    @return A string depending on: 
+              Valid input: The number in words.
+              Invalid input: an error message.
+    """
+
     if number.isdigit():
         return client.service.NumberToWords(ubiNum=int(number))
     return "Por favor, ingresa un número válido."
